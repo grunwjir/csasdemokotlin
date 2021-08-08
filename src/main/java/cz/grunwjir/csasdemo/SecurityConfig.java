203c1484dbc4d88647712a24938947f58701e509
Jiri Grunwald <jiri.grunwald@alza.cz>
2021-08-08 18:33:58 +0200

    [security] Add basic authentication to REST endpoints

---
+++ src/main/java/cz/grunwjir/csasdemo/SecurityConfig.java
@@ -0,0 +1,29 @@
+package cz.grunwjir.csasdemo;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Security configuration for REST API.
+ *
+ * @author Jiri Grunwald
+ */
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers(HttpMethod.OPTIONS, "/**")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+    }
+
+}
